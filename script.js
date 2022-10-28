4cd2c8ea9f4ff44ad22b7ba6c3ec2f81457399b5
expeditus303 <ricardotonietto@gmail.com>
2022-10-28 10:06:12 -0300

    slowness when get new messages adjusted

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let messages = [];
-const askName = prompt('Digite seu nome:')
+const askName = prompt('Type your nickname:')
 const serverName = {name: askName}
 
 const chat = document.querySelector('.chat');
@@ -8,9 +8,16 @@ const txtSendMessage = document.querySelector('.txtSendMessage')
 
 pushServerName()
 getData()
+setInterval(getData, 3000)
 
+// TO SEND NICKNAME TO THE SERVER
+function pushServerName() {
+    const promisse = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', serverName)
+    promisse.then(okay);
+    promisse.catch(notokay)
+}
 
-// TO SEND A MESSAGE
+// TO SEND A MESSAGE TO THE SERVER
 function sendMessage() {
     let sendMessageTemplate = { 
         from: askName,
@@ -25,53 +32,51 @@ function sendMessage() {
     promisse.catch(notokay)
 }
 
-
-
-
-function pushServerName() {
-    const promisse = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', serverName)
-    promisse.then(okay);
-    promisse.catch(notokay)
-}
-
-
-
+// IF EVERTHING IS ALLRIGHT ON SENDING NICKNAME TO THE SERVER
 function okay() {
     alert('tudo ok')
 }
-
+// IF HAVE SOME PROBLEM ON SENDING NICKNAME TO THE SERVER
 function notokay() {
     alert('nada ok')
 }
 
+// ASK FOR DATA ON THE SERVER
 function getData() {
     const promisse = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
     promisse.then(success);
     promisse.catch(error)
 }
 
-// ALL DATA RECEIVED IS IN dataReceived VARIABLE, BUT TO SEE THE MESSAGES IT'S NEEDED TO ACESS .DATA INSIDE ALL DATA -> dataReceived.data
+// DO IT WHEN DATA IS RECEIVED WITH SUCCESS AFTER ASKING. ALL DATA RECEIVED IS IN dataReceived VARIABLE, BUT TO SEE THE MESSAGES IT'S NEEDED TO ACESS .DATA INSIDE ALL DATA -> dataReceived.data
 function success(dataReceived) {
     messages = dataReceived.data;
     renderMessages()
 }
 
+// DO IT WHEN HAVE SOME PROBLEM RECEIVING DATA FROM SERVER
 function error() {
     alert('erro')
 }
 
 
+// TO RENDER MESSAGES FROM SERVER ON HTML
 function renderMessages() {
     
+    chat.innerHTML = ''
+
     for (let i = 0; i < messages.length; i++) {
+
+        
         
         const messageContainer = document.querySelectorAll('.messageContainer')
 
         
-
+        
         if (messages[i].type == 'status') {
             messages[i].type = '';
             messages[i].to = '' ;
+            
 
         } else if (messages[i].type == 'message') {
             messages[i].type = 'para'
@@ -87,16 +92,20 @@ function renderMessages() {
                 <span class="text">${messages[i].text}</span>
             </div>
         `
+        if (messages[i].type == 'status') {
+            for (let j = 0; j < messageContainer.length; j++) {
+                messageContainer[j].classList.add('status');
+            }
+        }
+        
+        
 
         automaticScroll();
     }
 }
 
-
-
-
-
+// TO AUTOMATIC SCROLL TO THE LAST MESSAGE
 function automaticScroll() {
-    const elementoQueQueroQueApareca = chat.lastElementChild
-    elementoQueQueroQueApareca.scrollIntoView();
+    const lastMessage = chat.lastElementChild
+    lastMessage.scrollIntoView();
 }
